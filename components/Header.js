f7939c5a98fe0b02eed5938aff6fb8845d7d2575
Riskjunk <colofulcooke@gmail.com>
2024-01-13 22:21:28 +0900

    🔥 不要なタグを削除

--- components/Header.js
+++ components/Header.js
@@ -15,58 +15,55 @@ export function Page() {
 
 export function HeaderTab() {
   return (
-    // Header.mainはテスト用のclass
-    <main>
-      <header>
-        <div className={Header.logo}>
-          <h1>ouseijin</h1>
-          <h2>O</h2>
-        </div>
-        <nav className={Header.nav}>
-          <ul>
-            <li>
-              <a href="#1">
-                <Image
-                src="/icon-1.png"
-                width={40}
-                height={40}
-                alt="Picture of the author"/>
-                <em>Homepage</em>
-              </a>
-            </li>
-            <li>
-              <a href="#2">
-                <Image
-                src="/icon-2.png"
-                width={40}
-                height={40}
-                alt="Picture of the author"/>
-                <em>About Us</em>
-              </a>
-            </li>
-            <li>
-              <a href="#3">
-                <Image
-                src="/icon-3.png"
-                width={40}
-                height={40}
-                alt="Picture of the author"/>
-                <em>Our Gallery</em>
-              </a>
-            </li>
-            <li>
-              <a href="#4">
-                <Image
-                src="/icon-4.png"
-                width={40}
-                height={40}
-                alt="Picture of the author"/>
-                <em>Contact Us</em>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </main>
+    <header>
+      <div className={Header.logo}>
+        <h1>ouseijin</h1>
+        <h2>O</h2>
+      </div>
+      <nav className={Header.nav}>
+        <ul>
+          <li>
+            <a href="#1">
+              <Image
+              src="/icon-1.png"
+              width={40}
+              height={40}
+              alt="Picture of the author"/>
+              <em>Homepage</em>
+            </a>
+          </li>
+          <li>
+            <a href="#2">
+              <Image
+              src="/icon-2.png"
+              width={40}
+              height={40}
+              alt="Picture of the author"/>
+              <em>About Us</em>
+            </a>
+          </li>
+          <li>
+            <a href="#3">
+              <Image
+              src="/icon-3.png"
+              width={40}
+              height={40}
+              alt="Picture of the author"/>
+              <em>Our Gallery</em>
+            </a>
+          </li>
+          <li>
+            <a href="#4">
+              <Image
+              src="/icon-4.png"
+              width={40}
+              height={40}
+              alt="Picture of the author"/>
+              <em>Contact Us</em>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
