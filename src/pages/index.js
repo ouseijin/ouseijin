d8984e1bd2caa9fc4ed50003968bf29591583ce3
Riskjunk <colofulcooke@gmail.com>
2024-02-07 21:35:45 +0900

    💄 自分自身の紹介する仮ページを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,6 +56,37 @@ export default function Home() {
           </div>
         </div>    
       </div>
+      <div class="slide" id="2">
+        <div class="content second-content">
+          <div id='tabs'>
+            <ul>
+              <li><a href='#tabs-1'><span class='fa fa-desktop'></span></a></li>
+              <li><a href='#tabs-2'><span class='fa fa-users'></span></a></li>
+              <li><a href='#tabs-3'><span class='fa fa-mobile'></span></a></li>
+            </ul>
+            <section class='tabs-content'>
+              <article id='tabs-1'>
+                <h2>What We Offer?</h2>
+                <span>Free Bootstrap v4 Theme</span>
+                <p>Earth is free Bootstrap v4.1.3 HTML Template for you. Feel free to edit and use this website template for any purpose. Please tell your friends about Tooplate website. Follow us on <a href="https://www.facebook.com/tooplate/">Facebook page</a> for latest updates. Thank you.</p>
+                <p>Aenean non suscipit risus. Nam mattis, ipsum ac aliquet dignissim, eros justo tempor dolor, rutrum feugiat nunc mauris et elit. Mauris sem odio, accumsan vel sem sed, aliquam convallis dui. Aenean et commodo est.</p>
+              </article>
+              <article id='tabs-2'>
+                <h2>What We Do?</h2>
+                <span>Ut nec metus eros</span>
+                <p>Nunc ultricies lectus et sem interdum molestie. Donec non sagittis velit. Vivamus vitae dictum metus, id imperdiet diam. Vestibulum elementum urna erat, eu tincidunt nisl pulvinar nec. Nulla a pharetra felis, sit amet placerat felis. Fusce a tincidunt dui.</p>
+                <p>Quisque blandit purus orci, et cursus ante porttitor in. Curabitur eleifend turpis id diam ultricies hendrerit. Donec et lectus vitae justo porta molestie.</p>
+              </article>
+              <article id='tabs-3'>
+                <h2>Who We Are?</h2>
+                <span>Etiam tempus ex ut mi</span>
+                <p>Vivamus dictum odio at enim posuere, et dapibus nunc sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> 
+                <p>Integer a egestas tellus, id malesuada velit. Pellentesque tincidunt, libero eu rutrum volutpat, nisi urna mollis felis, sed mollis sem libero at magna.</p>
+              </article>
+            </section>
+          </div>
+        </div>
+      </div>
       <main className={styles.main}>
         
         <div className={styles.description}>
